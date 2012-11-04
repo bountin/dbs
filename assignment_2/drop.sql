bf8f84c0ff0694b05c7072127862fa637adc8855
Martin Prebio <mp@25th-floor.com>
2012-11-04 16:00:35 +0100

    Dropping database

---
+++ assignment_2/drop.sql
@@ -0,0 +1,21 @@
+
+DROP TABLE bergefahrzeug;
+DROP TABLE loeschfahrzeug;
+
+DROP TABLE wettkampf_teilnahme;
+DROP TABLE wettkampf;
+DROP TABLE pers_wktruppe;
+DROP TABLE wettkampftruppe;
+
+DROP TABLE bericht;
+DROP TABLE einsatz;
+DROP TABLE ereignis;
+DROP TABLE fahrzeug;
+DROP TABLE fahrzeug_modell;
+
+ALTER TABLE person DROP mannschaft;
+ALTER TABLE mannschaft DROP leiter;
+
+DROP TABLE mannschaft;
+DROP TABLE person;
+DROP TABLE dienstgrad;
