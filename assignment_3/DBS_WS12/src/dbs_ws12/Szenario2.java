aa159a4770d75e8ff6a3f0cc304940191a0c4661
Martin Prebio <mp@25th-floor.com>
2012-12-02 19:46:11 +0100

    Finished Szenario 2

--- assignment_3/DBS_WS12/src/dbs_ws12/Szenario2.java
+++ assignment_3/DBS_WS12/src/dbs_ws12/Szenario2.java
@@ -12,11 +12,11 @@ public class Szenario2 {
     public static void main(String[] args) {
         if (args.length <= 6 && args.length >= 4) {
             /*
-             * args[0] ... type -> [a|b], 
-             * args[1] ... server, 
+             * args[0] ... type -> [a|b],
+             * args[1] ... server,
              * args[2] ... port,
-             * args[3] ... database, 
-             * args[4] ... username, 
+             * args[3] ... database,
+             * args[4] ... username,
              * args[5] ... password
              */
 
@@ -24,11 +24,11 @@ public class Szenario2 {
 
             if (args.length == 4) {
                 conn = DBConnector.getConnection(args[1], args[2], args[3]);
-            } 
+            }
             else {
                 if (args.length == 5) {
                     conn = DBConnector.getConnection(args[1], args[2], args[3], args[4], "");
-                } 
+                }
                 else {
                     conn = DBConnector.getConnection(args[1], args[2], args[3], args[4], args[5]);
                 }
@@ -44,7 +44,7 @@ public class Szenario2 {
                 }
             }
 
-        } 
+        }
         else {
             System.err.println("Ungueltige Anzahl an Argumenten!");
         }
@@ -73,44 +73,90 @@ public class Szenario2 {
          * Setzen Sie das aus Ihrer Sicht passende Isolation-Level:
          */
 
-        /*
-         * Abfrage 1:
-         * Ermitteln Sie Typ, Ort und die Anzahl betroffener Personen 
-         * aller Einsaetze, bei denen die Mannschaft mit der ID 10 
-         * beteiligt war und sortieren Sie das Ergebnis nach Anzahl
-         * der betroffenden Personen in absteigender Reihenfolge. 
-         * Geben Sie alle Daten auf der Konsole aus.
-         */
-            
-        /*
-         * Vorgegebener Codeteil
-         * ################################################################################
-         */
-         wait("Druecken Sie <ENTER> zum Fortfahren ...");
-        /*
-         * ################################################################################
-         */
+	    try {
+		    connection.setAutoCommit(false);
+		    connection.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
 
-        /*
-         * Abfrage 2:
-         * Ermitteln Sie zu jedem Typ ('Brand', 'Unfall', 'Hochwasser' oder 'Sonstiges', 
-         * zu dem bereits ein Ereignis gespeichert wurde, aus, wie viele Ereignisse es
-         * von diesem Typ bereits gab und sortieren Sie das Ergebnis nach der Summe 
-         * der Ereignisse absteigend. Geben Sie alle Daten auf der Konsole aus.
-         */
-            
-        /*
-         * Vorgegebener Codeteil
-         * ################################################################################
-         */
-        wait("Druecken Sie <ENTER> zum Beenden der Transaktion ...");
-        /*
-         * ################################################################################
-         */
-            
-        /*
-         * Beenden Sie die Transaktion
-         */
+		    /*
+			 * Abfrage 1:
+			 * Ermitteln Sie Typ, Ort und die Anzahl betroffener Personen
+			 * aller Einsaetze, bei denen die Mannschaft mit der ID 10
+			 * beteiligt war und sortieren Sie das Ergebnis nach Anzahl
+			 * der betroffenden Personen in absteigender Reihenfolge.
+			 * Geben Sie alle Daten auf der Konsole aus.
+			 */
+
+		    Statement stmt1 = connection.createStatement();
+		    ResultSet rs1 = stmt1.executeQuery(
+			    "SELECT typ, ort, betroffene_personen\n" +
+				    "FROM einsatz ei\n" +
+				    "JOIN ereignis er ON ei.ereig_id = er.id\n" +
+				    "WHERE ei.man_id = 10\n" +
+				    "ORDER BY betroffene_personen DESC\n" +
+				    ";"
+		    );
+
+		    System.out.println();
+		    System.out.printf("%15s %30s %4s\n", "Typ", "Ort", "Personen");
+		    System.out.println("-------------------------------------------------------");
+
+		    while (rs1.next()) {
+			    System.out.printf("%15s %30s %4d\n", rs1.getString(1), rs1.getString(2), rs1.getInt(3));
+		    }
+
+	        /*
+	         * Vorgegebener Codeteil
+	         * ################################################################################
+	         */
+	         wait("Druecken Sie <ENTER> zum Fortfahren ...");
+	        /*
+	         * ################################################################################
+	         */
+
+		    Statement stmt2 = connection.createStatement();
+		    ResultSet rs2 = stmt2.executeQuery(
+			    "SELECT typ, count(*) as anzahl, sum(betroffene_personen) as personen_summe\n" +
+			    "FROM ereignis\n" +
+			    "GROUP BY 1"
+		    );
+
+			System.out.println();
+		    System.out.printf("%15s %4s %7s\n", "Typ", "Anz.", "Personen");
+		    System.out.println("-----------------------------");
+
+		    while (rs2.next()) {
+			    System.out.printf("%15s %4d %7d\n", rs2.getString(1), rs2.getInt(2), rs2.getInt(3));
+		    }
+
+	        /*
+	         * Abfrage 2:
+	         * Ermitteln Sie zu jedem Typ ('Brand', 'Unfall', 'Hochwasser' oder 'Sonstiges',
+	         * zu dem bereits ein Ereignis gespeichert wurde, aus, wie viele Ereignisse es
+	         * von diesem Typ bereits gab und sortieren Sie das Ergebnis nach der Summe
+	         * der Ereignisse absteigend. Geben Sie alle Daten auf der Konsole aus.
+	         */
+
+	        /*
+	         * Vorgegebener Codeteil
+	         * ################################################################################
+	         */
+	        wait("Druecken Sie <ENTER> zum Beenden der Transaktion ...");
+	        /*
+	         * ################################################################################
+	         */
+
+	        /*
+	         * Beenden Sie die Transaktion
+	         */
+		    connection.commit();
+	    } catch (SQLException e) {
+		    try {
+			    connection.rollback();
+		    } catch (SQLException e1) {
+			    e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+		    }
+		    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+	    }
 
         System.out.println("Transaktion A Ende");
     }
@@ -127,18 +173,20 @@ public class Szenario2 {
         try {
             Statement stmt = connection.createStatement();
 
-            stmt.executeUpdate("INSERT INTO Ereignis (Zeitpunkt, Ort, Typ, AnzBPers) " +
-                               "VALUES ('2012-06-15 19:02', 'Hauptstrasse 18', " +
+            stmt.executeUpdate("INSERT INTO Ereignis (id, Zeitpunkt, Ort, Typ, betroffene_personen) " +
+                               "VALUES (5000, '2012-06-15 19:02', 'Hauptstrasse 18', " +
                                "'Brand', 10 )");
 
+	        stmt.executeUpdate("UPDATE ereignis SET typ = 'Sonstiges' WHERE id = 1");
+
             stmt.close();
-            
+
             System.out.println("Ein Ereignis wurde hinzugefuegt ...");
-            
+
             wait("Druecken Sie <ENTER> zum Beenden der Transaktion ...");
 
             connection.commit();
-        } 
+        }
         catch (SQLException ex) {
             Logger.getLogger(Szenario2.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -150,15 +198,15 @@ public class Szenario2 {
     }
 
     private static void wait(String message) {
-        /* 
-         * Vorgegebener Codeteil 
+        /*
+         * Vorgegebener Codeteil
          * ################################################################################
          */
         Scanner s = new Scanner(System.in);
         try {
             System.out.println(message);
             s.nextLine();
-        } 
+        }
         catch (Exception ex) {
             Logger.getLogger(Szenario2.class.getName()).log(Level.SEVERE, null, ex);
         }
